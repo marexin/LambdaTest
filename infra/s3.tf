583ed0c59357dc7e97e62229cb71c991047a27ab
Marek Pekarovic <marek.pekarovic@gmail.com>
2023-11-25 11:50:06 +0100

    Update s3.tf

--- infra/s3.tf
+++ infra/s3.tf
@@ -27,9 +27,7 @@ data "aws_iam_policy_document" "lambda" {
 
     actions = [
       "s3:Get*",
-      "s3:List*",
-      "s3:PutObject*",
-      "s3:PutObjectAcl*"
+      "s3:List*"
     ]
 
     resources = [
