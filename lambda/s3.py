88e9e46570f2dcd1fab7baa6b652047e7310163a
Marek Pekarovic <marek.pekarovic@gmail.com>
2023-11-25 13:51:44 +0100

    Update s3.py

--- lambda/s3.py
+++ lambda/s3.py
@@ -1,7 +1,36 @@
 import boto3
-import gunicorn
-import sqlalchemy as db
+import pandas as pd
 import os
+import io
 
 def lambda_handler(event, context):
     print('File was uploaded to S3 bucket')
+
+    # Initialize the S3 client
+    s3 = boto3.client('s3')
+
+    # Specify the S3 bucket and object key of the CSV file
+    bucket_name = 'lambda-layers-dev'
+    file_key = 'files/sample_file.csv'
+
+    try:
+        # Read the CSV file from S3
+        response = s3.get_object(Bucket=bucket_name, Key=file_key)
+        csv_content = response['Body'].read().decode('utf-8')
+
+        # Create a Pandas DataFrame
+        df = pd.read_csv(io.StringIO(csv_content))
+
+        # Example: Print the first 5 rows from DataFrame
+        print(df.head(5))
+
+        return {
+            'statusCode': 200,
+            'body': 'File read successfully into DataFrame.'
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
